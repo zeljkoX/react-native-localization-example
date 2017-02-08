4feaa12d5bffff3b25fc2179e515b7926002c5c4
Zeljko <zeljko89markovic@gmail.com>
2017-02-08 09:16:52 +0100

    Added support to unsuscribe on view unmount

--- src/components/languageProvider/connect.js
+++ src/components/languageProvider/connect.js
@@ -9,13 +9,22 @@ import  React, { Component, PropTypes } from 'react';
 
 const connect = (ComponentToWrap) => {
   return class LanguageComponent extends Component {
+    constructor(p,c){
+      super(p,c);
+      this.update = () => this.forceUpdate();
+    }
     static contextTypes = {
       strings: PropTypes.object.isRequired,
       language: PropTypes.object.isRequired
     }
+    
     componentDidMount():void {
     // subscribe to future theme changes
       this.subscribe();
+    }
+    componentWillUnmount():void {
+      // unsubscribe to changes
+      this.unsubscribe();
     }
     render():React.Element<any> {
       const { strings, language } = this.context;
@@ -24,7 +33,10 @@ const connect = (ComponentToWrap) => {
       );
     }
     subscribe = () => {
-      this.context.language.subscribe(() => this.forceUpdate());
+      this.context.language.subscribe(this.update);
+    }
+    unsubscribe = () => {
+      this.context.language.unsubscribe(this.update);
     }
   };
 };
